745742c9351121346d4c623c2e7a9605de2b5399
Joshua Francis <joshua.francis@live.com.au>
2020-06-12 18:01:42 +1000

    Added UI to modify terrain in game
    Added some sliders and buttons to mimic the options in the inspector.
    This is because I need to submit a exicutable version of the project in
    the assesment.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
+	public void Refresh()
+	{
+		GameData.worldGenerator.refresh = true;
+	}
+	
+	public void RandomiseSeed()
+	{
+		GameData.worldGenerator.randomiseSeed = true;
+	}
+
+	public void ChangeSeed(string newVal)
+	{
+		GameData.worldGenerator.seed = long.Parse(newVal);
+	}
+
+	public void ChangeWorldSize(string newVal)
+	{
+		GameData.worldGenerator.WorldSizeInChunks = int.Parse(newVal);
+	}
+
+	public void ChangeSmoothTerrain(bool newVal)
+	{
+		GameData.worldGenerator.smoothTerrain = newVal;
+	}
+
+	public void ChangeNoiseScale(float newVal)
+	{
+		GameData.worldGenerator.noiseScale = newVal;
+	}
+	public void ChangeNumOctave(float newVal)
+	{
+		GameData.worldGenerator.numOctaves = (int)newVal;
+	}
+}
